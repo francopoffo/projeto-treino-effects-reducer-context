18756c01f15acac1e55a5624a6e84c77ab9ee1c4
francopoffo <franco_sutter@yahoo.com.br>
2022-12-16 16:06:48 -0300

    Alterando idioma

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,7 +6,7 @@ import classes from './Home.module.css';
 const Home = (props) => {
   return (
     <Card className={classes.home}>
-      <h1>Welcome back!</h1>
+      <h1>Bem vindo de volta</h1>
     </Card>
   );
 };
